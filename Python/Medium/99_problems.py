c755fa5a8d000a5689a85273be5c00bd483bec85
hennas <32382243+hennas@users.noreply.github.com>
2024-02-25 15:36:03 +0200

    Updated formatting

--- Python/Medium/99_problems.py
+++ Python/Medium/99_problems.py
@@ -1,9 +1,16 @@
-import sys
+from sys import stdin, stdout
 
-price = next(sys.stdin).strip()
-price_99 = price[:-2] + '99'
-if len(price_99) > 2 and int(price_99) - int(price) > 50:
-    start = int(price[:-2]) - 1
-    sys.stdout.write(f'{start * 100 + 99}')
-else:
-    sys.stdout.write(price_99)
+
+def main():
+    price = next(stdin).strip()
+    price_99 = price[:-2] + '99'
+
+    if len(price_99) > 2 and int(price_99) - int(price) > 50:
+        start = int(price[:-2]) - 1
+        stdout.write(f'{start * 100 + 99}')
+    else:
+        stdout.write(price_99)
+
+
+if __name__ == '__main__':
+    main()
