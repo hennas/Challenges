1145701078ae709014b918636a227b488ea5e8d7
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-03-05 16:20:58 +0200

    Improved performance slightly

--- Python/Easy/oddmanout.py
+++ Python/Easy/oddmanout.py
@@ -4,7 +4,9 @@ def main():
     for case_num in range(cases):
         next(stdin)
         guest_nums = list(map(int, next(stdin).split()))
-        print(f'Case #{case_num+1}: {[guest_num for guest_num in guest_nums if guest_nums.count(guest_num) == 1][0]}')
+        for guest_num in guest_nums:
+            if guest_nums.count(guest_num) == 1:
+                print(f'Case #{case_num+1}: {guest_num}')
 
 
 if __name__ == '__main__':
