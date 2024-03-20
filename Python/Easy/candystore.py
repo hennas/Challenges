366062b825a13244bcc7442aabee65567d0a9ae8
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-03-20 15:31:55 +0200

    Shorter

--- Python/Easy/candystore.py
+++ Python/Easy/candystore.py
@@ -1,7 +1,8 @@
-n,q=map(int,input().split())
-n=[input().split()for _ in range(n)]
-q={input()+str(i):[]for i in range(q)}
+x=input
+n,q=map(int,x().split())
+n=[x().split()for _ in range(n)]
+q={x()+str(i):[]for i in range(q)}
 for i in q:
  for j in n:
-  if j[0][0]+j[1][0]in i:q[i].append(' '.join(j))
+  if j[0][0]+j[1][0]in i:q[i]+=[' '.join(j)]
 for v in q.values():l=len(v);print('nobody'if l==0 else['ambiguous',v[0]][l==1])
