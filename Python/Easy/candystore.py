c9ad7fba652574186b599c68f17f690d76eefb49
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-03-20 15:32:52 +0200

    Shorter...!

--- Python/Easy/candystore.py
+++ Python/Easy/candystore.py
@@ -1,6 +1,6 @@
 x=input
 N,Q=map(int,x().split())
 N=[x()for _ in range(N)]
-Z=[z[0][0]+z[1][0]for z in[n.split()for n in N]]
+Z=[''.join(next(zip(*z.split())))for z in N]
 Q=[x()for _ in range(Q)]
 for q in Q:c=Z.count(q);print(['ambiguous',N[Z.index(q)]][c==1]if c else'nobody')
